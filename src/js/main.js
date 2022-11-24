01c2ea762182bb81327ee145986e11cec26674c9
Maxim <titov3000@mail.ru>
2022-11-24 19:59:27 +0300

    dragAndDrop

--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import burger from './modules/burger';
 import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -28,4 +29,5 @@ window.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
     burger('.burger', '.burger-menu');
     scrolling('.pageup');
+    drop();
 });
